9a2431bf5cbfdf32221de708e0f3ad45e486a6ad
ivandohan <srgivando@gmail.com>
2022-10-04 08:39:14 +0700

    Greening

---
+++ K4a/Shape.java
@@ -0,0 +1,9 @@
+package K4a;
+
+public abstract class Shape extends Object {
+    public double getArea() {return 0.0;}
+
+    public double getVolume() {return 0.0;}
+
+    public abstract String getName();
+}
